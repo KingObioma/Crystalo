b170068aa96d8997f0a4b06e04ab825a7f35819b
king <kelvinozak0@gmail.com>
2025-12-27 19:32:02 +0100

    Increase Auth Security

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -35,7 +35,6 @@ class pagesController extends Controller
 
     public function projects(){
         $projects = project::limit(9)->get();
-        $projects = project::limit(9)->get();
         return view('frontend.pages.projects', compact('projects'));
     }
 
