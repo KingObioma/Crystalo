e6ff19581e110e08b62957824066c266f71b9e1f
king <kelvinozak0@gmail.com>
2025-12-27 19:40:29 +0100

    Increase Auth Security

--- app/Http/Controllers/crystaloController.php
+++ app/Http/Controllers/crystaloController.php
@@ -276,7 +276,7 @@ class crystaloController extends Controller
         $product = Product::find($id);
 
         if (!$product) {
-            return redirect()->back()->with('error', 'Product not found');
+            return redirect()->back()->with('error', 'goods not found');
         }
 
         return view('backend.pages.edit_product', compact('product'));
